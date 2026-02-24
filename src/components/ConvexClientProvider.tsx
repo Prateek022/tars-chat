6111bb02bc23b4e97604f7b29b2ca7cf2d89ba9c
Prateek <prateek.ps988@gmail.com>
2026-02-24 23:54:47 +0530

    Hardcode Convex URL to fix deployment

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -3,24 +3,16 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
-import { useEffect, useState } from "react";
+
+const convex = new ConvexReactClient(
+  "https://benevolent-penguin-669.convex.cloud"
+);
 
 export default function ConvexClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [convex, setConvex] = useState<ConvexReactClient | null>(null);
-
-  useEffect(() => {
-    const client = new ConvexReactClient(
-      process.env.NEXT_PUBLIC_CONVEX_URL!
-    );
-    setConvex(client);
-  }, []);
-
-  if (!convex) return null;
-
   return (
     <ClerkProvider>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
